06f25ad2a40aaeed08679434dee1a327f5d080c3
antaga04 <antaga04@gmail.com>
2023-06-10 13:02:02 +0200

    Add nav component

---
+++ src/components/nav/nav.js
@@ -0,0 +1,13 @@
+import './nav.css';
+
+export const Nav = () => {
+  return `
+  <nav>
+    <div id="nav-logo">
+      <a href="#">
+        <i class="fa-solid fa-circle"></i>
+      </a>
+    </div>
+  </nav>
+  `;
+};
